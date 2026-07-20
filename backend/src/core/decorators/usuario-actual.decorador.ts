7396c1bb517f9e8f262867e065086b25d41deb01
Omar Mariscal <omarmariscal543@gmail.com>
2026-07-20 16:15:20 -0600

    fix(backend): Corrección al JwtPayLoad y el decorador UsuarioActual() para habilitar el paso de una interfaz Actor para validar permisos

--- backend/src/core/decorators/usuario-actual.decorador.ts
+++ backend/src/core/decorators/usuario-actual.decorador.ts
@@ -1,14 +1,30 @@
 // src/core/decorators/usuario-actual.decorador.ts
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtPayloadDto } from '../auth/dto/jwt-payload.dto';
+import { crearActor } from '@domain/roles/actor.factory';
+import { SesionUsuario } from '../interfaces/sesion-usuario.interface';
 
 export const UsuarioActual = createParamDecorator(
-  (data: keyof JwtPayloadDto | undefined, ctx: ExecutionContext) => {
+  (data: keyof SesionUsuario | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user = request.user as JwtPayloadDto;
+    const jwtPayload = request.user as JwtPayloadDto;
 
-    // Si pasaste un parámetro (ej. @UsuarioActual('rol')), te da solo ese string.
-    // Si no pasaste nada (ej. @UsuarioActual()), te da todo el objeto.
-    return data ? user?.[data] : user;
+    // Si por alguna razón el Guard no inyectó el user (ej. endpoint público), retornamos null
+    if (!jwtPayload) return null;
+
+    // 1. Construimos el Actor de Dominio usando tu Factory
+    const actorDominio = crearActor(
+      jwtPayload.rol,
+      jwtPayload.permisos_especiales,
+    );
+
+    // 2. Armamos el objeto rico de Sesión
+    const sesionCompleta: SesionUsuario = {
+      ...jwtPayload,
+      actor: actorDominio,
+    };
+
+    // 3. Retornamos la propiedad solicitada o el objeto completo
+    return data ? sesionCompleta[data] : sesionCompleta;
   },
 );
